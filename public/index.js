0ef6bb19b8d7e5a7bbb795609782ef19bd26e36d
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-04-03 03:28:57 +1300

    Added file context menu

--- public/index.js
+++ public/index.js
@@ -20,7 +20,8 @@ const fileTemplate = document.getElementById('file-template');
 const fileActionErrorTemplate = document.getElementById('file-action-error-template');
 const lostConnectionDiv = document.getElementById('lost-connection');
 const socketReconnectButton = document.getElementById('socket-reconnect');
-const pulsatingAnimation = 'pulsating 1.5s infinite'
+const pulsatingAnimation = 'pulsating 1.5s infinite';
+const fileContextScreenBlocker = document.getElementById('file-context-screen-blocker');
 
 function showFileActions() {
     if (fileActionsShowing === false) {
@@ -274,15 +275,29 @@ function createFile(file) {
     fileElement.querySelector('.file').id = `file-${fileId}`
     fileElement.querySelector('.file-filename').textContent = file.fileName;
     fileElement.querySelector('.file-filesize').textContent = SizeCalculator(file.fileSize);
-    fileElement.querySelector('.file-delete-button').setAttribute('onclick', `deleteFile('${fileId}', '${file.fileName}')`)
-    fileElement.querySelector('.file-download-button').setAttribute('onclick', `downloadFile('${fileId}', '${file.fileName}')`)
     fileElement.querySelector('.file-context-button').setAttribute('onclick', `openContextMenu('${fileId}', '${file.fileName}')`)
 
     fileList.appendChild(fileElement)
 }
 
 function openContextMenu(fileId, fileName) {
-    console.log(document.getElementById(`file-${fileId}`).getBoundingClientRect())
+    document.querySelector('.file-context-menu')?.remove();
+    fileContextScreenBlocker.style.display = 'block';
+
+    const boundingRect = document.getElementById(`file-${fileId}`).querySelector('div').getBoundingClientRect()
+    console.log(boundingRect);
+    const box = document.getElementById('file-menu-template').content.cloneNode(true);
+    const div = box.querySelector('.file-context-menu');
+    div.querySelector('.file-delete-button').setAttribute('onclick', `deleteFile('${fileId}', '${fileName}')`)
+    div.querySelector('.file-download-button').setAttribute('onclick', `downloadFile('${fileId}', '${fileName}')`)
+    div.style.top = `${boundingRect.top}px`;
+    div.style.left = `${boundingRect.right}px`;
+    document.body.append(box)
+}
+
+function closeFileContextMenu() {
+    document.querySelector('.file-context-menu')?.remove();
+    fileContextScreenBlocker.style.display = 'none';
 }
 
 function retryGettingFiles() {
@@ -358,6 +373,8 @@ function createFileAction(fileId, filename, progressBarText) {
 function deleteFile(fileId, fileName) {
     if (document.getElementById(`file-action-${fileId}`)) return
 
+    closeFileContextMenu()
+
     createFileAction(fileId, fileName, 'Waiting for server...')
     axios.delete(`/auth/file/${fileId}`).catch(error => {
         console.error(error)
@@ -367,6 +384,8 @@ function deleteFile(fileId, fileName) {
 
 function downloadFile(fileId, fileName) {
     if (document.getElementById(`file-action-${fileId}`)) return
+    
+    closeFileContextMenu()
     
     createFileAction(fileId, fileName, 'Waiting for server...')
     axios.get(`/auth/file/${fileId}`, {responseType: 'blob'}).then(response => {
