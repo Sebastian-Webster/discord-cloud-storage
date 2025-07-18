e347369571b42a20b5206dc294fc10465870dcdb
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2025-07-18 18:10:33 +0400

    update concurrent and max retry counts

--- libraries/Deleter.ts
+++ libraries/Deleter.ts
@@ -18,8 +18,8 @@ async function promiseFactory(messageId: string): Promise<void> {
 export function DeleteFile(userId: mongoose.Types.ObjectId, fileId: string, fileName: string, fileSize: number, messageIds: string[]): Promise<void> {
     return new Promise(async (resolve, reject) => {
         const messagesToDelete = messageIds.length;
-        const maxConcurrentPromises = 3;
-        const maxDeletionRetries = messageIds.length * 2;
+        const maxConcurrentPromises = 2;
+        const maxDeletionRetries = messageIds.length * 5;
         let promisesRunning = 0;
         let errorOccurred = false;
         let messagesDeleted = 0;
