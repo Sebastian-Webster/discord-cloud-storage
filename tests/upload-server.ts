9a53f8461a5e48b8f2b1019ebcee7fd5dfb8344f
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2025-07-20 12:41:59 +0400

    add upload APIs

--- tests/upload-server.ts
+++ tests/upload-server.ts
@@ -1,14 +1,15 @@
-import express from 'express'
-import rateLimit from 'express-rate-limit'
-import os from 'os'
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import os from 'os';
 import fs from 'fs';
-import path from 'path';
+import fsPromises from 'fs/promises';
 
-const storageFolder = path.normalize(os.tmpdir() + '/dcstestserver')
+const storageFolder = os.tmpdir() + '/dcstestserver'
 
 const app = express();
 
 app.use(express.json())
+app.use(express.raw({type: 'application/octet-stream'}))
 
 const messages = new Map();
 const attachments = new Set();
@@ -26,7 +27,40 @@ app.post('/api/v10/channels/:channelId/messages/attachments', (req, res) => {
     res.json(response)
 })
 
-app.put('/upload/:filename', (req, res) => {
+app.put('/upload/:filename', async (req, res) => {
+    if (!Buffer.isBuffer(req.body)) {
+        return res.status(400).send('Body must be a Buffer.')
+    }
 
+    const filename = req.params.filename;
+    const storePath = `${storageFolder}/${filename}`;
+
+    if (fs.existsSync(storePath)) {
+        return res.status(400).send('File already exists.')
+    }
+
+    await fsPromises.writeFile(`${storageFolder}/${req.params.filename}`, req.body)
+
+    attachments.add(filename)
+
+    res.status(200)
+})
+
+app.post('/api/v10/channels/:channelId/messages', (req, res) => {
+    const body = req.body;
+
+    const files = body.map(item => item.upload_filename)
+
+    for (const file of files) {
+        if (!attachments.has(file)) {
+            return res.status(404).send(`File ${file} does not exist.`)
+        }
+    }
+
+    const messageId = crypto.randomUUID();
+
+    messages.set(messageId, files)
+
+    res.status(200).json({id: messageId})
 })
 
